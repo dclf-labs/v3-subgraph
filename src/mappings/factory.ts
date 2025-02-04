be38d734f7f7eecd5fab67dfa49c1e7fbda01b18
pedro-noon <pierre@dclflabs.xyz>
2025-02-04 20:44:10 +0700

    zksync

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -129,7 +129,7 @@ export function handlePoolCreatedHelper(
 
   pool.token0 = token0.id
   pool.token1 = token1.id
-  pool.feeTier = BigInt.fromI32(event.params.fee)
+  pool.feeTier = BigInt.fromI32(event.params.tickSpacing)
   pool.createdAtTimestamp = event.block.timestamp
   pool.createdAtBlockNumber = event.block.number
   pool.liquidityProviderCount = ZERO_BI
