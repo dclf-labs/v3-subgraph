9ed3998dce8d83e61661e14181d356025f18e9ba
pedro-noon <pierre@dclflabs.xyz>
2025-02-19 16:58:08 +0700

    sohpon

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,9 +3,9 @@ import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x9D63d318143cF14FF05f8AAA7491904A494e6f13'
+export const FACTORY_ADDRESS = '0x0f6e27007e257e74c86522387BD071D561ba3C97'
 export const ALLOWED_POOL_ADDRESSES = [
-    '0x12bf23c2fe929c23ab375199efad425e70c0ece1'  
+    '0x0Cdb3454293FDfa187B14025F29cdA3319fcd3B5','0x9116a0E6C8d04E82397B64E72D26d14D290b42eF'  
   ].map<string>(a => a.toLowerCase())
   
 export let ZERO_BI = BigInt.fromI32(0)
